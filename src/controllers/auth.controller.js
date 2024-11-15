ea23343821e2693f21036791add9183ccb0563e0
Willis "Thiên Phúc" Crympt <phucplaymc@gmail.com>
2024-11-15 14:46:08 +0700

    asnd nakojosaindka

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -12,7 +12,22 @@ require('dotenv').config();
 
 
 //Genearte unique user ID
-u
+const generateUserId = async () => {
+  const prefix = "ND";
+  let isUnique = false;
+  let userId = "";
+
+  while (!isUnique) {
+      const random = Math.floor(10000000 + Math.random() * 90000000);
+      userId = `${prefix}${random}`;
+      const existingUser = await User.findOne({ userId });
+      if(!existingUser) {
+        isUnique = true;
+      }
+  }
+
+  return userId;
+};
 
 // Generate a unique driver ID
 const generateDriverId = async () => {
